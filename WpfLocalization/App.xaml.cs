6874c9eb60c77da5b3b9c084bbb3d7265bf7a109
David Mordechai <david.mordechai79@gmail.com>
2022-05-20 11:40:54 +0300

    Add project files.

---
+++ WpfLocalization/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfLocalization;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("he-IL");
+        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("he-IL");
+    }
+}
---
+++ WpfLocalization/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WpfLocalization;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var currentLanguage = System.Globalization.CultureInfo.CurrentCulture.IetfLanguageTag;
+        if (currentLanguage.ToLower().StartsWith("he"))
+            FlowDirection = FlowDirection.RightToLeft;
+    }
+}
